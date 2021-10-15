1a71bc973414749b2b07e6aef6049a29b2124c3b
ifanz22 <ifanz22@bss.on.ca>
2021-10-15 19:57:39 -0400

    The data from the PointRecord collection is now presented in the table

--- public/PointRecord.js
+++ public/PointRecord.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 import { collection, getDocs } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
-
+import { doc, setDoc } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
 
 // setting up my web app framework - use your apiKey and info
 const firebaseApp = initializeApp({
@@ -14,12 +14,14 @@ const firebaseApp = initializeApp({
 // setting up the fireStore database
 const db = getFirestore();
 let houses = [];
+let pointRecord = [];
 
 class House{ //Creating a custom class called "House"
-  constructor(name, points, head){ //Has the following properties
+  constructor(name, points, head, colour){ //Has the following properties
     this.name = name;
     this.points = points;
     this.head = head;
+    this.colour = colour;
   }
   addPoints(points){
     this.points += points; //Add points to total
@@ -27,8 +29,15 @@ class House{ //Creating a custom class called "House"
   delPoints(points){
     this.points -= points; //Take away points from total
   }
-  reset(){
-    this.points = 0; //Reset point number to 0
+}
+
+class Record{
+  constructor(house, user, points, date, description){
+    this.house = house;
+    this.user = user;
+    this.points = points;
+    this.date = date;
+    this.description = description;
   }
 }
 
@@ -39,8 +48,15 @@ const querySnapshot = await getDocs(collection(db, "Houses")); //Querying for th
 querySnapshot.forEach((doc) => {
   console.log(`${doc.id} => ${doc.data()}`);
   console.log(doc.data().points);
-  houses.push(new House(doc.data().name, doc.data().points, doc.data().head)); //Putting database info into the local array
+  houses.push(new House(doc.id, doc.data().points, doc.data().head, doc.data().colour)); //Putting database info into the local array
   console.log(houses);
+});
+
+// getting the data from a collection called "PointRecord"
+const querySnapshotRecord = await getDocs(collection(db, "PointRecord")); //Querying for the collection called "PointRecord"
+querySnapshotRecord.forEach((doc) => {
+  pointRecord.push(new Record(doc.data().house, doc.data().name, doc.data().pointsAdded, doc.data().date, doc.data().description));
+  console.log(pointRecord);
 });
 
 for(let i = 0; i < 12; i++){
@@ -58,6 +74,7 @@ function addPoints(){
   let frm = document.getElementById(form);
   let houseName = frm.elements["house"].value;
   let pAdd = frm.elements["pointsAdded"].value;
+  let pointDescription = frm.elemnts["pointDescription"].value;
   let oldPoints;
   //Adding points to the right house
   let house = db.collection("Houses").doc(houseName); //Saving the right document into a variable
@@ -67,7 +84,7 @@ function addPoints(){
       db.collection("Houses").doc(houseName).set(
         //Changes the data in the database, adding the data we got from the form to the one from the database
         {name: houseName, points: oldPoints + pAdd}
-      )
+      );
     }else{
       // doc.data() will be undefined in this case
       console.log("No such document!");
@@ -75,6 +92,16 @@ function addPoints(){
   }).catch(function(error) {
     console.log("Error getting document:", error);
   });
+  // Add a new document in collection "PointRecord"
+  await setDoc(doc(db, "PointRecord", "user"), {
+    date: "Today's Date",
+    description: pointDescription,
+    pointsAdded: pAdd,
+    house: houseName,
+    name: "user";
+  });
+  //Adding the entry to the database
+
   document.getElementById("pointsForm").style.visibility = "hidden";
 }
 
@@ -85,3 +112,35 @@ function showPointsForm(){
 function cancelAdd(){
   document.getElementById("pointsForm").style.visibility = "hidden";
 }
+
+function pointDisplay(){ //Displays the point record table
+  for(let i in pointRecord){
+    let tbl = document.getElementById("pointsTable");
+    let rowNumber = tbl.rows.length;
+    let row = tbl.insertRow(rowNumber);
+    let hCell = row.insertCell(0); //House name column
+    let nCell = row.insertCell(1); //Point giver name column
+    let pCell = row.insertCell(2); //Number of points column
+    let daCell = row.insertCell(3); //Date column
+    let deCell = row.insertCell(4); //Description column
+    let delCell = row.insertCell(5); //Delete column
+    //Accessing info
+    hCell.innerHTML = pointRecord[i].house;
+    nCell.innerHTML = pointRecord[i].user;
+    pCell.innerHTML = pointRecord[i].points;
+    deCell.innerHTML = pointRecord[i].description;
+    daCell.innerHTML = pointRecord[i].date;
+    //Creating delete button
+    let deleteButton = document.createElement("BUTTON");
+    let dt = document.createTextNode("X");
+    deleteButton.appendChild(dt);
+    //Delete action wrapped in second function so that it does not automatically trigger
+    deleteButton.addEventListener("click", function(){
+      //houses[rowNumber-1].deleteEntry(rowNumber-1);
+    });
+    delCell.appendChild(deleteButton);
+  }
+  console.log(pointRecord);
+}
+
+pointDisplay();
